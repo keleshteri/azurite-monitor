ad45bb4c1616f554969d831a1b40f45899a0f8de
Mohammad Mehdi Shaban Keleshteri <mehdi.shaban.keleshteri@outlook.com>
2024-05-04 22:27:12 +0800

    chore: Update monitor.js to use environment variables for file path and event listener URL

--- monitor.js
+++ monitor.js
@@ -2,56 +2,58 @@ const axios = require("axios");
 const chokidar = require("chokidar");
 const fs = require("fs");
 require("dotenv").config();
-//env variables
-const path = process.env.PATH_TO_METADATA_FILE;
-// env variables
-const localEventUrl = process.env.LOCAL_EVENT_LISTENER_URL;
-const watcher = chokidar.watch(path, { persistent: true });
 
-watcher.on("change", () => {
-  console.log(`Detected change in metadata file: ${path}`);
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      console.error("Failed to read file:", err);
-      return;
-    }
-    try {
-      const json = JSON.parse(data);
-      const blobs = json.collections.find(
-        (c) => c.name === "$BLOBS_COLLECTION$"
-      ).data;
-
-      blobs.sort(
-        (a, b) =>
-          new Date(b.properties.lastModified) -
-          new Date(a.properties.lastModified)
-      );
-
-      if (blobs.length > 0) {
-        const latestBlob = blobs[0];
-        const message = createServiceBusMessage(latestBlob);
-        console.log(latestBlob);
-        console.log("Sending message to local event listener...");
-        console.log(JSON.stringify(message, null, 2));
-        console.log(`Latest Blob Name: ${latestBlob.name}`);
-        console.log(`Creation Time: ${latestBlob.properties.creationTime}`);
-        console.log(`Last Modified: ${latestBlob.properties.lastModified}`);
-        console.log(`Content Length: ${latestBlob.properties.contentLength}`);
-        console.log(`Blob Type: ${latestBlob.properties.blobType}`);
-        axios
-          .post(localEventUrl, message)
-          .then((response) =>
-            console.log("Event sent successfully:", response.data)
-          )
-          .catch((error) => console.error("Error sending event:", error));
-      } else {
-        console.log("No blobs found.");
+function startMonitoring() {
+  const path = process.env.PATH_TO_METADATA_FILE;
+  const localEventUrl = process.env.LOCAL_EVENT_LISTENER_URL;
+  const watcher = chokidar.watch(path, { persistent: true });
+  watcher.on("change", () => {
+    console.log(`Detected change in metadata file: ${path}`);
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        console.error("Failed to read file:", err);
+        return;
       }
-    } catch (parseError) {
-      console.error("Error parsing JSON:", parseError);
-    }
+      try {
+        const json = JSON.parse(data);
+        const blobs = json.collections.find(
+          (c) => c.name === "$BLOBS_COLLECTION$"
+        ).data;
+
+        blobs.sort(
+          (a, b) =>
+            new Date(b.properties.lastModified) -
+            new Date(a.properties.lastModified)
+        );
+
+        if (blobs.length > 0) {
+          const latestBlob = blobs[0];
+          const message = createServiceBusMessage(latestBlob);
+          console.log(latestBlob);
+          console.log("Sending message to local event listener...");
+          console.log(JSON.stringify(message, null, 2));
+          console.log(`Latest Blob Name: ${latestBlob.name}`);
+          console.log(`Creation Time: ${latestBlob.properties.creationTime}`);
+          console.log(`Last Modified: ${latestBlob.properties.lastModified}`);
+          console.log(`Content Length: ${latestBlob.properties.contentLength}`);
+          console.log(`Blob Type: ${latestBlob.properties.blobType}`);
+          axios
+            .post(localEventUrl, message)
+            .then((response) =>
+              console.log("Event sent successfully:", response.data)
+            )
+            .catch((error) => console.error("Error sending event:", error));
+        } else {
+          console.log("No blobs found.");
+        }
+      } catch (parseError) {
+        console.error("Error parsing JSON:", parseError);
+      }
+    });
   });
-});
+  console.log(`Watching for changes in: ${path}`);
+}
+
 //
 // Function to create a Service Bus message from a blob
 function createServiceBusMessage(blob) {
@@ -83,4 +85,5 @@ function createServiceBusMessage(blob) {
     },
   };
 }
-console.log(`Watching for changes in: ${path}`);
+
+startMonitoring();
